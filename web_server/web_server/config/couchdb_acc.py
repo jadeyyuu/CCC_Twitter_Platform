48699ff4d9a43f5d18121e95dab98ad0de82789c
Jade <jadeyyuu@gmail.com>
2021-05-26 04:13:17 +0800

    update webserver

---
+++ web_server/web_server/config/couchdb_acc.py
@@ -0,0 +1,34 @@
+def instance_1_ip():
+    return '172.26.130.42'
+
+
+def instance_1_acc_name():
+    return 'admin'
+
+
+def instance_1_pass():
+    return 'password'
+
+
+def instance_2_ip():
+    return '172.26.131.236'
+
+
+def instance_2_acc_name():
+    return 'admin'
+
+
+def instance_2_pass():
+    return 'password'
+
+
+def instance_3_ip():
+    return '172.26.134.19'
+
+
+def instance_3_acc_name():
+    return 'admin'
+
+
+def instance_3_pass():
+    return 'password'
